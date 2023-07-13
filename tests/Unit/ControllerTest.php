a6c74ca4a05942e45fe79c44a65f7a763f7de936
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2023-07-13 14:00:00 +0200

    Confirm that ranking data for team Lyon displays as expected in table

--- tests/Unit/ControllerTest.php
+++ tests/Unit/ControllerTest.php
@@ -31,13 +31,8 @@ class ControllerTest extends TestCase
         $response = $this->get('/');
         $response->assertStatus(200);
         $response->assertSeeTextInOrder(['N°', 'Équipe', 'MJ', 'G', 'N', 'P', 'BP', 'BC', 'DB', 'PTS']);
-
-
-        /*
-
-        $response->assertSeeTextInOrder(['N°', 'Équipe', 'MJ', 'G', 'N', 'P', 'BP', 'BC', 'DB', 'PTS']);
         $response->assertSeeTextInOrder([2, 'Lyon', 38, 19, 6, 13, 111, 97, 14, 63]);
-    }*/
+    }
 
     public function testShowTeam()
     {
