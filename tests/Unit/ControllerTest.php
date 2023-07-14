d69eb007b64c9cb5e0e30946e593ea6799bb6abb
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2023-07-14 18:00:00 +0200

    Verify ranking table headers are displayed in correct order

--- tests/Unit/ControllerTest.php
+++ tests/Unit/ControllerTest.php
@@ -66,11 +66,12 @@ class ControllerTest extends TestCase
         }
         $response = $this->get('/teams/4');
         $response->assertStatus(200);
+        $response->assertSeeTextInOrder(['N°', 'Équipe', 'MJ', 'G', 'N', 'P', 'BP', 'BC', 'DB', 'PTS']);
+            
     }
         /*
 
 
-            $response->assertSeeTextInOrder(['N°', 'Équipe', 'MJ', 'G', 'N', 'P', 'BP', 'BC', 'DB', 'PTS']);
             $response->assertSeeTextInOrder([2, 'Lyon', 38, 19, 6, 13, 111, 97, 14, 63]);
             $response->assertSeeTextInOrder(['2048-08-03 00:00:00', 'Lyon', 3, 5, 'Angers']);
     }*/
