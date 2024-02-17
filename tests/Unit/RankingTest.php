d16a2f1dfd6d0acac9a158cfe0303fd460512ff6
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-02-17 10:00:00 +0100

    Add use TestGoalDifference class to test this method

--- tests/Unit/RankingTest.php
+++ tests/Unit/RankingTest.php
@@ -5,6 +5,13 @@ use App\Repositories\Ranking;
 use Tests\TestCase;
 class RankingTest extends TestCase
 {
+    function testGoalDifference():void
+    {
+        $ranking = new Ranking();
+        $this->assertEquals($ranking->goalDifference(2, 3), -1);
+        $this->assertEquals($ranking->goalDifference(0, 0), 0);
+        $this->assertEquals($ranking->goalDifference(4, 1), 3);
 
+    }
 
 }
