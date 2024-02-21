f3f907aa381a6f068e89d8bd43e6349227582a96
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-02-21 10:00:00 +0100

    add teamLosesMatch method implementation with one line

--- app/Repositories/Ranking.php
+++ app/Repositories/Ranking.php
@@ -11,30 +11,40 @@ class Ranking
 
     function points(int $matchWonCount, int $drawMatchCount): int
     {
-        return $matchWonCount*3+ $drawMatchCount;
+        return $matchWonCount * 3 + $drawMatchCount;
     }
 
     function teamWinsMatch(int $teamId, array $match): bool
     {
 
-        return ($teamId == $match['team0'] && $match['score0'] >$match['score1']) || ($teamId == $match['team1'] && $match['score1'] >$match['score0'] );
-       /* if ($teamId == $match['team0']) {
-            return $match['score0'] > $match['score1'];
-        }
+        return ($teamId == $match['team0'] && $match['score0'] > $match['score1']) || ($teamId == $match['team1'] && $match['score1'] > $match['score0']);
+        /* if ($teamId == $match['team0']) {
+             return $match['score0'] > $match['score1'];
+         }
 
-        if ($teamId == $match['team1']) {
-            return $match['score1'] > $match['score0'];
-        }
+         if ($teamId == $match['team1']) {
+             return $match['score1'] > $match['score0'];
+         }
 
-        return false; // Return false if the team ID doesn't match either team */
+         return false; // Return false if the team ID doesn't match either team */
     }
+
     function teamLosesMatch(int $teamId, array $match): bool
     {
-        return false;
+        return ($teamId == $match['team0'] && $match['score0'] < $match['score1']) || ($teamId == $match['team1'] && $match['score1'] < $match['score0'] );
+//        if ($teamId == $match['team0']) {
+//            return $match['score0'] < $match['score1'];
+//        }
+//
+//        if ($teamId == $match['team1']) {
+//            return $match['score1'] < $match['score0'];
+//        }
+//
+//        return false;
     }
-    function teamDrawsMatch(int $teamId, array $match): bool
-    {
-        return false;
-    }
+        function teamDrawsMatch(int $teamId, array $match): bool
+        {
+            return false;
+        }
 
 }
