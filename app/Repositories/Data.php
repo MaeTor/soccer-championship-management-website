490afbae4ad47cf5b1be11cbba928559aa56cb11
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-02-23 06:00:00 +0100

    add teams method that returns an array of the teams inside the competition

--- app/Repositories/Data.php
+++ app/Repositories/Data.php
@@ -4,5 +4,29 @@ namespace App\Repositories;
 
 class Data
 {
-
+    function teams()
+    {
+        return [
+            ['id' => 1, 'name' => 'Paris'],
+            ['id' => 2, 'name' => 'Marseille'],
+            ['id' => 3, 'name' => 'Lyon'],
+            ['id' => 4, 'name' => 'Toulouse'],
+            ['id' => 5, 'name' => 'Nice'],
+            ['id' => 6, 'name' => 'Nantes'],
+            ['id' => 7, 'name' => 'Strasbourg'],
+            ['id' => 8, 'name' => 'Montpellier'],
+            ['id' => 9, 'name' => 'Bordeaux'],
+            ['id' => 10, 'name' => 'Lille'],
+            ['id' => 11, 'name' => 'Rennes'],
+            ['id' => 12, 'name' => 'Reims'],
+            ['id' => 13, 'name' => 'Le Havre'],
+            ['id' => 14, 'name' => 'Gap'],
+            ['id' => 15, 'name' => 'Toulon'],
+            ['id' => 16, 'name' => 'Grenoble'],
+            ['id' => 17, 'name' => 'Dijon'],
+            ['id' => 18, 'name' => 'Angers'],
+            ['id' => 19, 'name' => 'Nîmes'],
+            ['id' => 20, 'name' => 'Villeurbanne']
+        ];
+    }
 }
