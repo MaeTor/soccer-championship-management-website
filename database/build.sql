0c53b4f8d8406d370c87082195d41293dfc7c1b7
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-03-05 12:00:00 +0100

    add the table matches with its structure in the database

--- database/build.sql
+++ database/build.sql
@@ -2,3 +2,13 @@ CREATE TABLE teams(
                       id INTEGER PRIMARY KEY AUTOINCREMENT,
                       name VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE matches
+(
+    id     INTEGER PRIMARY KEY AUTOINCREMENT,
+    team0  INTEGER NOT NULL,
+    team1  INTEGER NOT NULL,
+    score0 INTEGER NOT NULL,
+    score1 INTEGER NOT NULL,
+    date   DATETIME
+);
