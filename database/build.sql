ec027d8a0deb04419b2b6f5c8f3f757c6f79b7d8
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-03-05 15:00:00 +0100

    add integrity and unicity constraints for the tables matches

--- database/build.sql
+++ database/build.sql
@@ -3,12 +3,15 @@ CREATE TABLE teams(
                       name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE matches
-(
+CREATE TABLE matches(
     id     INTEGER PRIMARY KEY AUTOINCREMENT,
     team0  INTEGER NOT NULL,
     team1  INTEGER NOT NULL,
     score0 INTEGER NOT NULL,
     score1 INTEGER NOT NULL,
-    date   DATETIME
-);
+    date   DATETIME,
+    FOREIGN KEY (team0) REFERENCES teams(id),
+    FOREIGN KEY (team1) REFERENCES teams(id),
+    UNIQUE (team0, team1),
+    CHECK(team0 != team1)
+    );
