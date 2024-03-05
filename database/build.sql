1fb772a5c10c0d6566a65b7023d3b648ccafce0b
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-03-05 21:00:00 +0100

    add the table ranking with its structure in the database

--- database/build.sql
+++ database/build.sql
@@ -15,3 +15,17 @@ CREATE TABLE matches(
     UNIQUE (team0, team1),
     CHECK(team0 != team1)
     );
+
+CREATE TABLE ranking
+(
+    team_id            INTEGER PRIMARY KEY,
+    rank               INTEGER NOT NULL,
+    match_played_count INTEGER NOT NULL,
+    match_won_count    INTEGER NOT NULL,
+    match_lost_count   INTEGER NOT NULL,
+    draw_count         INTEGER NOT NULL,
+    goal_for_count     INTEGER NOT NULL,
+    goal_against_count INTEGER NOT NULL,
+    goal_difference    INTEGER NOT NULL,
+    points             INTEGER NOT NULL
+);
