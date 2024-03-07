b0e6868a5cf78740bb5898513877b89cc53f7f63
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-03-07 21:00:00 +0100

    add use declarations to use DB static class (facade)

--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -2,7 +2,14 @@
 
 namespace App\Repositories;
 
+use Illuminate\Support\Facades\DB;
+
 class Repository
 {
+    function createDatabase(): void
+    {
+        DB::unprepared(file_get_contents('database/build.sql'));
+    }
+
 
 }
