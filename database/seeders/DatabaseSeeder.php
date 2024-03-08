f1ef6ad21f25fe087ec58868264afd4432ec83ac
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-03-08 08:00:00 +0100

    add touch statement in run method of Databaseseeder to allow the command php artisan db:seed and thus the createDatabase() executed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        touch('database/database.sqlite');
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
