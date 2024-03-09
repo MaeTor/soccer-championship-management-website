30db009f5a821ce3e40f0a29d63a86b5b45913c3
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-03-09 12:00:00 +0100

    add insertteam method in the class Repository

--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -11,5 +11,15 @@ class Repository
         DB::unprepared(file_get_contents('database/build.sql'));
     }
 
+    function insertTeam(array $team): int
+    {
+        if (array_key_exists('id', $team)) {
+            DB::table('teams')->insert($team);
+            return $team['id'];
+        }
+        return DB::table('teams')->insertGetId($team);
+    }
+
+
 
 }
