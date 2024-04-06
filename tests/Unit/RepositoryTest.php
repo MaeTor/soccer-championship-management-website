a28fd94fa13bec18e9e6c4b491965ba178838afc
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-04-06 13:00:00 +0200

    populate database with teams and matches

--- tests/Unit/RepositoryTest.php
+++ tests/Unit/RepositoryTest.php
@@ -75,6 +75,8 @@ class RepositoryTest extends TestCase
     function testMatchIfExists(): void
     {
         // Remplir la base de données avec les équipes et les matchs
+        $this->repository->fillDatabase();
+
 
     }
 
