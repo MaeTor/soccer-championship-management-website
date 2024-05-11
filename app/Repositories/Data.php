c09f5596a6dd1e966ca6ed59dbcca30265c3b53d
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-05-11 13:00:00 +0200

    add match entry for id 240 in expectedMatchesForTeam

--- app/Repositories/Data.php
+++ app/Repositories/Data.php
@@ -521,6 +521,7 @@ class Data
             ['id' => 208, 'team0' => 4, 'team1' => 14, 'score0' => 4, 'score1' => 1, 'date' => '2049-01-04 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Gap'],
             ['id' => 219, 'team0' => 2, 'team1' => 4, 'score0' => 4, 'score1' => 2, 'date' => '2049-01-11 00:00:00', 'name0' => 'Marseille', 'name1' => 'Toulouse'],
             ['id' => 230, 'team0' => 4, 'team1' => 1, 'score0' => 5, 'score1' => 0, 'date' => '2049-01-18 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Paris'],
+            ['id' => 240, 'team0' => 4, 'team1' => 6, 'score0' => 5, 'score1' => 4, 'date' => '2049-01-25 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Nantes'],
 
     }
 }
