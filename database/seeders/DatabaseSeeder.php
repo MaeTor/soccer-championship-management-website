a518f29b03dbdf89e6627cba96564d33b67ae1c5
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-05-17 11:00:00 +0200

    update database seeding to insert full ranking based on Data class calculations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,9 +18,8 @@ class DatabaseSeeder extends Seeder
         touch('database/database.mysql');
         $repository = new Repository();
         $repository->createDatabase();
-        $repository->insertTeam(['name' => 'Marseille']);
-        $repository->insertTeam(['name' => 'Nice']);
-         $repository->insertMatch([ 'team0' => 1, 'team1' => 2, 'score0' => 2, 'score1' => 5, 'date' => '2048-08-03 00:00:00']);
+        $repository->fillDatabase();
+        $repository->updateRanking();
 
         // \App\Models\User::factory(10)->create();
 
