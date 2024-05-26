39788dfb440524ac47916d3e6739ab00e984e74a
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-05-26 14:00:00 +0200

    Added showTeam method to return team matches data based on team ID, to prepare data for further processing and display.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,7 @@ use App\Repositories\Repository;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
@@ -23,9 +24,10 @@ class Controller extends BaseController
     }
 
 
-    public function showTeam(int $teamId):string
+    public function showTeam(int $teamId)
     {
-        return "showTeam $teamId";
+        $teamMatches = $this->repository->teamMatches($teamId);
+        return $teamMatches;
     }
 
 }
