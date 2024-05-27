326064e5ee182bd4ed0e7c5f0961ef68fca6176e
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-05-27 10:00:00 +0200

    Add HTML structure for the ranking table with headers for team stats

--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -2,6 +2,25 @@
 <html>
     <head></head>
     <body>
+        <table>
+            <thead>
+            <tr>
+                <th>N°</th>
+                <th>Équipe</th>
+                <th>MJ</th>
+                <th>G</th>
+                <th>N</th>
+                <th>P</th>
+                <th>BP</th>
+                <th>BC</th>
+                <th>DB</th>
+                <th>PTS</th>
+            </tr>
+            </thead>
+            <tbody>
+            <!-- Les lignes des équipes seront insérées ici -->
+            </tbody>
+        </table>
         @json($ranking)
     </body>
 </html>
