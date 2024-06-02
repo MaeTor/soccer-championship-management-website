2c7ad171c58051626387d924a65054d8991ea7a6
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-06-02 08:30:00 +0200

    create the structure of the view team.blade.php

---
+++ resources/views/team.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        th{
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: center;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+<table>
+    <thead>
+    </thead>
+    <tbody>
+    <!-- Les lignes des équipes seront insérées ici -->
+    @foreach ($teamMatches as $match)
+        <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+    @endforeach
+    </tbody>
+</table>
+</body>
+</html>
