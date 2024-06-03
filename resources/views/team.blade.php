12cf9d93cd69733308eddf91e9d5a0f9cde49345
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-06-03 10:30:00 +0200

    Add iteration over date data to populate table rows dynamically

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -26,11 +26,12 @@
     <!-- Les lignes des équipes seront insérées ici -->
     @foreach ($teamMatches as $match)
         <tr>
+            <th>{{$match['date']}}</th>
             <th></th>
             <th></th>
             <th></th>
             <th></th>
-            <th></th>
+        </tr>
     @endforeach
     </tbody>
 </table>
