316f0bb2f1b5c43355345a9b46324cbbf3415042
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-06-06 10:30:00 +0200

    Add url for each team1 in the template team.blade.php (fixed-2)

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -34,7 +34,7 @@
             <th><a href="{{route('teams.show', ['teamId'=>$match['team0'] ] ) }}">{{$match['name0']}}</a></th>
             <th>{{$match['score0']}}</th>
             <th>{{$match['score1']}}</th>
-            <th><a href="{{route('teams.show'), ['teamId'=>$match['team1'] ] }}">{{$match['name1']}}</a></th>
+            <th><a href="{{route('teams.show', ['teamId'=>$match['team1'] ] }}">{{$match['name1']}}</a></th>
         </tr>
     @endforeach
     </tbody>
