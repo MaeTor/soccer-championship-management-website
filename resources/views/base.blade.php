e257dc93fc2edafaed242853b844d188a1fc501c
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-06-12 12:20:00 +0200

    Add the tailwind style of the first div in the top of the rank

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,7 +4,7 @@
         <title>@yield('title')</title>
     </head>
     <body>
-    <div>
+    <div class="border-b shadow-sm p-3 md:px-4 mb-3">
         <div>
             <h5>@yield('title')</h5>
             <nav>
