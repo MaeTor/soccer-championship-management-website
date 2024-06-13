ec9874d50dcbc0e4c85be641ccc9b60e5b704446
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-06-13 11:00:00 +0200

    Add nav, and both a tags style corresponding to classement and connexion buttons

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -10,10 +10,10 @@
     <div class="border-b shadow-sm p-3 md:px-4 mb-3">
         <div class="container mx-auto flex flex-col md:flex-row items-center">
             <h5 class="text-lg font-normal mr-auto">@yield('title')</h5>
-            <nav>
-                <a href="/">Classement</a>
+            <nav class="my-2 md:my-0 md:mr-3">
+                <a href="/" class="px-2 text-gray-800 hover:text-gray-600">Classement </a>
             </nav>
-            <a href="/login">Connexion</a>
+            <a class="border border-blue-500 text-blue-500 px-4 py-2 rounded hover:bg-blue-500 hover:text-white transition-colors" href="/login">Connexion</a>
         </div>
     </div>
     <div>
