3b42330acad54befaf32cd8116bca9f46ed68076
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-06-14 08:00:00 +0200

    Add section 'title' to ranking blade

--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -1,26 +1,14 @@
 @extends('base')
+
+    @section('title')
+        Classement
+    @endsection
 <!doctype html>
 <html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         @vite('resources/css/app.css')
-        <style>
-            table {
-                width: 100%;
-                border-collapse: collapse;
-            }
-
-            th{
-                border: 1px solid #ddd;
-                padding: 8px;
-                text-align: center;
-            }
-
-            th {
-                background-color: #f2f2f2;
-            }
-        </style>
     </head>
     <body>
         <table>
