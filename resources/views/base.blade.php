a259587ffd883afd4816efa5f636287d2bbdd9dd
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-06-15 12:00:00 +0200

    remove space in base blade

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -11,7 +11,7 @@
         <div class="container mx-auto flex flex-col md:flex-row items-center">
             <h5 class="text-lg font-normal mr-auto">@yield('title')</h5>
             <nav class="my-2 md:my-0 md:mr-3">
-                <a href="/" class="px-2 text-gray-800 hover:text-gray-600">Classement </a>
+                <a href="/" class="px-2 text-gray-800 hover:text-gray-600">Classement</a>
             </nav>
             <a class="border border-blue-500 text-blue-500 px-4 py-2 rounded hover:bg-blue-500 hover:text-white transition-colors" href="/login">Connexion</a>
         </div>
