176a5c99ac5f93e441b4b61ba07a90e974fac1da
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-07-02 16:20:00 +0200

    Define expected sortedRanking return array in mock

--- tests/Unit/ControllerTest.php
+++ tests/Unit/ControllerTest.php
@@ -7,6 +7,21 @@ class ControllerTest extends TestCase
     public function testShowRanking()
     {
         $this->mock(Repository::class, function ($mock) {
+            $mock->shouldReceive('sortedRanking')->once()->andReturn([
+                [
+                    'rank' => 2,
+                    'name' => 'Lyon',
+                    'team_id' => 3,
+                    'match_played_count' => 38,
+                    'won_match_count' => 19,
+                    'lost_match_count' => 13,
+                    'draw_match_count' => 6,
+                    'goal_for_count' => 111,
+                    'goal_against_count' => 97,
+                    'goal_difference' => 14,
+                    'points' => 63
+                ]
+            ]);
 
         }
     }
