fc5963fe27d62e4927452c41a2b17ffa0f623256
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-07-11 13:00:00 +0200

    add comment to explain how to create a mock of Repository

--- tests/Unit/ControllerTest.php
+++ tests/Unit/ControllerTest.php
@@ -9,6 +9,7 @@ class ControllerTest extends TestCase
 {
     public function testShowRanking()
     {
+        // Créer un mock de la classe Repository
         $repositoryMock = Mockery::mock(Repository::class);
         $repositoryMock->shouldReceive('sortedRanking')->once()->andReturn([
             'rank' => 2,
@@ -24,22 +25,7 @@ class ControllerTest extends TestCase
             'points' => 63
         ]);
 
-        /*function ($mock) {
-            $mock->shouldReceive('sortedRanking')->once()->andReturn([
-                [
-                    'rank' => 2,
-                    'name' => 'Lyon',
-                    'team_id' => 3,
-                    'match_played_count' => 38,
-                    'won_match_count' => 19,
-                    'lost_match_count' => 13,
-                    'draw_match_count' => 6,
-                    'goal_for_count' => 111,
-                    'goal_against_count' => 97,
-                    'goal_difference' => 14,
-                    'points' => 63
-                ]
-            ]);
+        /*
         };
         $response = $this->get('/');
         $response->assertStatus(200);
