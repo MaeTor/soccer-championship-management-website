0d0200a3f2d4aacd5b6a251bc697a71a41efa860
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-07-14 16:00:00 +0200

    add get calling method that returns an object which represents the server response

--- tests/Unit/ControllerTest.php
+++ tests/Unit/ControllerTest.php
@@ -63,15 +63,11 @@ class ControllerTest extends TestCase
                     'name1' => 'Angers'
                 ]
             ]);
-
         }
+        $response = $this->get('/teams/4');
     }
-        /*$this->mock(Repository::class, function ($mock) {
-            $data = new Data();
+        /*
 
-
-        });
-            $response = $this->get('/teams/4');
             $response->assertStatus(200);
             $response->assertSeeTextInOrder(['N°', 'Équipe', 'MJ', 'G', 'N', 'P', 'BP', 'BC', 'DB', 'PTS']);
             $response->assertSeeTextInOrder([2, 'Lyon', 38, 19, 6, 13, 111, 97, 14, 63]);
