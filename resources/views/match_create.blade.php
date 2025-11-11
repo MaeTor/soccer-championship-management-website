66f48caaeb69259357f9b3715c05f5fe201dc291
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2025-11-11 19:19:44 +0100

    Add Tailwind-based form and Blade template
    Added match_create.blade.php containing the form UI and add custom tailwind classes to style the form in app.css

---
+++ resources/views/match_create.blade.php
@@ -0,0 +1,33 @@
+@extends('base')
+@section('title', 'Création d\'un match')
+@section('content')
+<form>
+    <div class="form-group">
+        <label for="team0">Équipe à domicile</label>
+        <select class="form-control" id="team0" name="team0">
+        </select>
+    </div>
+    <div class="form-group">
+        <label for="team1">Équipe à l'extérieur</label>
+        <select class="form-control" id="team1" name="team1">
+        </select>
+    </div>
+    <div class="form-group">
+        <label for="date">Date</label>
+        <input type="date" class="form-control" id="date" name="date">
+    </div>
+    <div class="form-group">
+        <label for="time">Heure</label>
+        <input type="time" class="form-control" id="time" name="time">
+    </div>
+    <div class="form-group">
+        <label for="score0">Nombre de buts de l'équipe à domicile</label>
+        <input type="number" class="form-control" id="score0" name="score0" min="0">
+    </div>
+    <div class="form-group">
+        <label for="score1">Nombre de buts de l'équipe à l'extérieur</label>
+        <input type="number" class="form-control" id="score1" name="score1" min="0">
+    </div>
+    <button type="submit" class="btn btn-primary">Soumettre</button>
+</form>
+@endsection
