19336f22e68db347c0ed3145d12ceee301424a15
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2025-11-11 21:54:34 +0100

    add FormTest to test the two forms math_create and team_create

---
+++ tests/Unit/FormTest.php
@@ -0,0 +1,333 @@
+<?php
+
+namespace Tests\Unit;
+
+use Exception;
+use Tests\TestCase;
+use App\Repositories\Repository;
+use App\Repositories\Data;
+
+class FormTest extends TestCase
+{
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->repository = new Repository();
+        $this->repository->createDatabase();
+    }
+
+    public function testCreateTeamRedirectsIfUserIsNotAuthenticated()
+    {
+        $response = $this->get('/teams/create');
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
+
+    public function testStoreTeam()
+    {
+        $this->mock(Repository::class, function ($mock) {
+            $mock->shouldReceive('insertTeam')->with(['name' => 'Marseille'])->once()->andReturn(1);
+            $mock->shouldReceive('updateRanking')->once();
+        });
+        $response = $this->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/teams', ['team_name' => 'Marseille']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/teams/1');
+    }
+
+    public function testStoreTeamRedirectsIfUserIsNotAuthenticated()
+    {
+        $response = $this->post('/teams', ['team_name' => 'Marseille']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
+
+    public function testStoreTeamRedirectsIfNameIsAbsent()
+    {
+        $response = $this->withHeader('Referer', '/teams/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/teams', []);
+        $response->assertStatus(302);
+        $response->assertRedirect('/teams/create');
+        $response->assertSessionHasErrors(["team_name"=>"Vous devez saisir un nom d'équipe."]);
+    }
+
+    public function testStoreTeamRedirectsIfNameIsTooShort()
+    {
+        $response = $this->withHeader('Referer', '/teams/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/teams', ['team_name' => 'A']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/teams/create');
+        $response->assertSessionHasErrors(["team_name"=>"Le nom doit contenir au moins 3 caractères."]);
+    }
+
+    public function testStoreTeamRedirectsIfNameIsTooLong() 
+    {
+        $response = $this->withHeader('Referer', '/teams/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/teams', ['team_name' => 'AAAAAAAAAAAAAAAAAAAAA']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/teams/create');
+        $response->assertSessionHasErrors(["team_name"=>"Le nom doit contenir au plus 20 caractères."]);
+    }
+
+    public function testStoreTeamRedirectsIfNameAlreadyExists() 
+    {    
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $response = $this->withHeader('Referer', '/teams/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/teams', ['team_name' => 'Marseille']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/teams/create');
+        $response->assertSessionHasErrors(["team_name"=>"Le nom d'équipe existe déjà."]);
+    }
+
+    public function testStoreTeamRedirectsIfRepositoryThrowsException() 
+    {    
+        $this->mock(Repository::class, function ($mock) {
+            $mock->shouldReceive('insertTeam')->andThrow(new Exception(""));
+        });
+        $response = $this->withHeader('Referer', '/teams/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/teams', ['team_name' => 'Marseille']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/teams/create');
+    }
+
+    public function testCreateMatchRedirectsIfUserIsNotAuthenticated()
+    {
+        $response = $this->get('/matches/create');
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
+
+    public function testStoreMatch()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $this->mock(Repository::class, function ($mock) {
+            $mock->shouldReceive('insertMatch')->with(['team0' => 1, 'team1' => 2, 'date'=>'2048-10-05 10:22', 'score0'=>2, 'score1'=>4])->once()->andReturn(1);
+            $mock->shouldReceive('updateRanking')->once();
+        });
+        $response = $this->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0' => '1', 'team1' => '2', 'date'=>'2048-10-05', 'time'=>'10:22', 'score0'=>'2', 'score1'=>'4']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/');
+    }
+
+    public function testStoreMatchRedirectsIfUserIsNotAuthenticated()
+    {
+        $response = $this->post('/matches', ['team0' => '1', 'team1' => '2', 'date'=>'2048-10-05', 'time'=>'10:22', 'score0'=>'2', 'score1'=>'4']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
+
+
+    public function testStoreMatchRedirectsIfTeam0IsAbsent()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team1' => '2', 'date'=>'2048-10-05', 'time'=>'10:22', 'score0'=>'2', 'score1'=>'4']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["team0"=>"Vous devez choisir une équipe."]);
+    }
+
+    public function testStoreMatchRedirectsIfTeam0DoesNotExist()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0' => '3', 'team1' => '2', 'date'=>'2048-10-05', 'time'=>'10:22', 'score0'=>'2', 'score1'=>'4']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["team0"=>"Vous devez choisir une équipe qui existe."]);
+    }
+
+    public function testStoreMatchRedirectsIfTeam1IsAbsent()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0' => '2', 'date'=>'2048-10-05', 'time'=>'10:22', 'score0'=>'2', 'score1'=>'4']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["team1"=>"Vous devez choisir une équipe."]);
+    }
+
+    public function testStoreMatchRedirectsIfTeam1DoesNotExist()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0' => '2', 'team1' => '3', 'date'=>'2048-10-05', 'time'=>'10:22', 'score0'=>'2', 'score1'=>'4']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["team1"=>"Vous devez choisir une équipe qui existe."]);
+    }
+    
+    public function testStoreMatchRedirectsIfDateIsAbsent()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0'=>'1', 'team1' => '2', 'time'=>'10:22', 'score0'=>'2', 'score1'=>'4']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["date"=>"Vous devez choisir une date."]);
+    }
+
+    public function testStoreMatchRedirectsIfDateIsNotValid()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0'=>'1', 'team1' => '2', 'date'=>'aaa', 'time'=>'10:22', 'score0'=>'2', 'score1'=>'4']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["date"=>"Vous devez choisir une date valide."]);
+    }
+
+    public function testStoreMatchRedirectsIfTimeIsAbsent()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0'=>'1', 'team1' => '2', 'date'=>'2048-10-05', 'score0'=>'2', 'score1'=>'4']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["time"=>"Vous devez choisir une heure."]);
+    }
+
+    public function testStoreMatchRedirectsIfTimeIsNotValid()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0'=>'1', 'team1' => '2', 'date'=>'2048-10-05', 'time'=>'aaa', 'score0'=>'2', 'score1'=>'4']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["time"=>"Vous devez choisir une heure valide."]);
+    }
+
+    public function testStoreMatchRedirectsIfScore0IsAbsent()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0'=>'1', 'team1' => '2', 'date'=>'2048-10-05', 'time'=>'10:22', 'score1'=>'4']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["score0"=>"Vous devez choisir un nombre de buts."]);
+    }
+
+    public function testStoreMatchRedirectsIfScore0IsNotInteger()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0'=>'1', 'team1' => '2', 'date'=>'2048-10-05', 'time'=>'10:22', 'score0'=>'a', 'score1'=>'4']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["score0"=>"Vous devez choisir un nombre de buts entier."]);
+    }
+
+    public function testStoreMatchRedirectsIfScore0IsNegative()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0'=>'1', 'team1' => '2', 'date'=>'2048-10-05', 'time'=>'10:22', 'score0'=>'-1', 'score1'=>'4']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["score0"=>"Vous devez choisir un nombre de buts entre 0 et 50."]);
+    }
+
+    public function testStoreMatchRedirectsIfScore0IsTooLarge()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0'=>'1', 'team1' => '2', 'date'=>'2048-10-05', 'time'=>'10:22', 'score0'=>'51', 'score1'=>'4']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["score0"=>"Vous devez choisir un nombre de buts entre 0 et 50."]);
+    }
+
+    public function testStoreMatchRedirectsIfScore1IsAbsent()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0'=>'1', 'team1' => '2', 'date'=>'2048-10-05', 'time'=>'10:22', 'score0'=>'4']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["score1"=>"Vous devez choisir un nombre de buts."]);
+    }
+
+    public function testStoreMatchRedirectsIfScore1IsNotInteger()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0'=>'1', 'team1' => '2', 'date'=>'2048-10-05', 'time'=>'10:22', 'score0'=>'4', 'score1'=>'a']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["score1"=>"Vous devez choisir un nombre de buts entier."]);
+    }
+
+    public function testStoreMatchRedirectsIfScore1IsNegative()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])        
+                         ->post('/matches', ['team0'=>'1', 'team1' => '2', 'date'=>'2048-10-05', 'time'=>'10:22', 'score0'=>'5', 'score1'=>'-1']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["score1"=>"Vous devez choisir un nombre de buts entre 0 et 50."]);
+    }
+
+    public function testStoreMatchRedirectsIfScore1IsTooLarge()
+    {
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0'=>'1', 'team1' => '2', 'date'=>'2048-10-05', 'time'=>'10:22', 'score0'=>'5', 'score1'=>'51']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+        $response->assertSessionHasErrors(["score1"=>"Vous devez choisir un nombre de buts entre 0 et 50."]);
+    }
+
+    public function testStoreMatchRedirectsIfRepositoryThrowsException() 
+    {    
+        $this->repository->insertTeam(['name' => 'Marseille']);
+        $this->repository->insertTeam(['name' => 'Paris']);
+        $this->mock(Repository::class, function ($mock) {
+            $mock->shouldReceive('insertMatch')->andThrow(new Exception(""));
+        });
+        $response = $this->withHeader('Referer', '/matches/create')
+                         ->withSession(['user' => ['id'=>1, 'email'=>'test@example.com']])
+                         ->post('/matches', ['team0'=>'1', 'team1' => '2', 'date'=>'2048-10-05', 'time'=>'10:22', 'score0'=>'5', 'score1'=>'5']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/matches/create');
+    }
+}
