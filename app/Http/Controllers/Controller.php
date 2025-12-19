f9ca1c88b87da2db97360b94288ee678a76ffdb8
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2025-12-19 10:49:59 +0100

    update TODO 1 and TODO 2 declarations to fit exactly with what the login method have to do

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -128,8 +128,8 @@ class Controller extends BaseController
         ];
         $validatedData = $request->validate($rules, $messages);
         try {
-            // TODO: Verify the user's credentials using $repository
-            // TODO: Authenticate the user and start the session
+            // TODO 1: Throw an exception if the user's password is incorrect
+            // TODO 2: Remember the user's authentication
         } catch (Exception $e) {
             return redirect()->back()->withInput()->withErrors('Impossible de vous authentifier.');
         }
