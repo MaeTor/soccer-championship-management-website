9b13c5c263d0bd2058cf25992061b5649cf6ba97
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2026-01-09 12:45:50 +0100

    implement front-end to delete a match in the team.blade.php (that contains a svg and a popover to confirm the suppression of the match)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+  const th = document.getElementById("delete-th");
+  const popover = document.getElementById("delete-popover");
+
+  th.addEventListener("click", () => {
+    popover.showPopover();
+  });
